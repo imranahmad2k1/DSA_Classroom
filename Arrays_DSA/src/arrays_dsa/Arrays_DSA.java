962c06ab868eabbec8ac2ee0a394836771587373
Allen Ghost <meetimrang@gmail.com>
2020-09-30 10:01:26 +0500

    Added Arrays Lab Program

--- Arrays_DSA/src/arrays_dsa/Arrays_DSA.java
+++ Arrays_DSA/src/arrays_dsa/Arrays_DSA.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package arrays_dsa;
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +16,42 @@ public class Arrays_DSA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int sum=0;
+        int high=0;
+        int low=0;
+        int num, total, avg;
+        System.out.print("Total Tests: ");
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int array[] = new int[N];
+        for(int i=0;i<N;++i){
+            System.out.println("Marks in Test " + (i+1));
+            num = sc.nextInt();
+            array[i]=num;
+            sum=sum+num;
+        }
+        
+        total = sum;
+        avg = total/N;
+        for (int i:array){
+            if (i>high){
+                high = i;
+            }
+            low = high;
+            if (i<low){
+                low = i;
+            }
+        }
+        
+        
+       
+        System.out.println("Student Transcript");
+        System.out.println("-------------------");
+        System.out.println("Total Marks: "+total);
+        System.out.println("Average Marks: "+avg);
+        System.out.println("Highest Marks: "+high);
+        System.out.println("Lowest Marks: "+low);
+        
     }
     
 }
